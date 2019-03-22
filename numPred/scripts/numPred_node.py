ff26ada2a78ec46aa7a99bf18317629b60b6ba2c
Yu Zhou <yu.nico.zhou@gmail.com>
2019-03-22 10:02:57 +0000

    1st commit

---
+++ numPred/scripts/numPred_node.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+from os.path import realpath, dirname, join
+from numPred.msg import out_msg
+import keras as K
+
+class numPredNode(object):
+    def __init__(self):
+        super(numPredNode,self).__init__()
+        model_path="/home/nvidia/yzchen_ws/ros_ws/src/numPred/models/numberClassify_Redmodel_min.h5"
+
+        print("loading model from"+model_path)
+        rospy.init_node('num_predictor',anonymous=True)
+        self.model=K.models.load_model(model_path)
+        self.pub_num=rospy.Publisher('/numPred/num',out_msg,queue_size=1)
+        self.sub_image=rospy.Subscriber("/armor_detector/num_roi",Image,self.img_callback)
+        print("initialize done")
+        rospy.spin()
+
+    def img_callback(self, data):
+        try:
+            # Convert image to numpy array
+            self.current_image = self._bridge.imgmsg_to_cv2(data, "bgr8")
+
+        except CvBridgeError as e:
+            rospy.logerr(e)
+
+        num=self.pred_number(self,self.current_image)
+        pred_out=out_msg()
+        pred_out.num=num
+        self.pub_num.publish(pred_out)
+
+    def pred_number(self,img):
+        testImg_a=np.array(img)
+        num=7
+        # pred=self.model.predict(testImg_a)
+        # num = np.argmax(pred)
+        return num
+
+ 
+def main():
+    """ main function
+    """
+    node = numPredNode()
+
+if __name__ == '__main__':
+    main()
---
+++ windMill/src/main.cpp
@@ -0,0 +1,99 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "MarkerSensor.h"
+static const std::string OPENCV_WINDOW = "Image window";
+using namespace std;
+using namespace cv;
+Mat img_src,bgrImg;
+Size dist_size = Size(640, 480);
+int X_bias, Y_bias, pix_x, pix_y;
+int status=0,lastStatus=0;
+    bool isSuccess = 0;
+  float X = 0, Y = 0, Z = 0;
+  int led_type = 0, false_idx = 0, frameCnt = 0, capIdx = 1;
+  MarkSensor markSensor;
+
+int frame_process(Mat &srcImg)
+{
+
+
+    /// detect and track
+    resize(srcImg, bgrImg, dist_size);
+    // bgrImg=srcImg.clone();
+    isSuccess = markSensor.ProcessFrameLEDXYZ(bgrImg, X, Y, Z, led_type,
+                                                pix_x, pix_y);
+       if (!isSuccess) {
+      status = 1;
+      cout << "detected target--------------" << endl;
+      X_bias = pix_x - bgrImg.cols/2;
+      Y_bias = pix_y - bgrImg.rows/2;
+       }else
+       {
+         status=0;
+       }
+
+}
+class ImageConverter
+{
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
+  image_transport::Publisher image_pub_;
+
+public:
+  ImageConverter()
+    : it_(nh_)
+  {
+    // Subscrive to input video feed and publish output video feed
+    image_sub_ = it_.subscribe("/camera/image_raw", 1,
+      &ImageConverter::imageCb, this);
+    image_pub_ = it_.advertise("/armor_detector/armor_roi", 1);
+
+    cv::namedWindow("detection result");
+  }
+
+  ~ImageConverter()
+  {
+    cv::destroyWindow("detection result");
+  }
+
+  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  {
+    
+    try
+    {
+      img_src = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8)->image.clone();
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    frame_process(img_src);
+
+    // Update GUI Window
+    cv::imshow("detection result", markSensor.img_show);
+    if(!markSensor.img_out.empty())
+      cv::imshow("feed to number", markSensor.img_out);
+    cv::waitKey(1);
+
+    // Output modified video stream
+    sensor_msgs::ImagePtr img_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", markSensor.img_out).toImageMsg();
+    img_msg->header.stamp = ros::Time::now();
+    image_pub_.publish(img_msg);
+  }
+};
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "image_converter");
+  ImageConverter ic;
+  ros::spin();
+  return 0;
+}
